a7f2875774319e7987490a0f01ce5c7852d275ab
Arthur <arthurschiller10@gmail.com>
2024-04-17 18:35:20 -0300

    metade

--- ap1/ap1.ino
+++ ap1/ap1.ino
@@ -20,11 +20,21 @@
 #define S3 12
 #define S4 13
 
+#define LED1 15
+#define LED2 16
+#define LED3 17
+#define LED4 18
+#define LED5 19
+
+#define LED5 11
+#define CHT 10
+
+#define B1 5
 
 
 
 int VAL1 = 0;
-
+int VAL2 = 0;
 
 int converteDecimal(int A0, int A1, int A2, int A3, int A4){
  int decimal = 0;
@@ -37,22 +47,45 @@ void setup() {
   pinMode(CH1,INPUT);
   pinMode(VED,OUTPUT);
   pinMode(VEM,OUTPUT);
+  pinMode(LED5, OUTPUT);
+  pinMode(CHT, INPUT);
+  pinMode(B1,OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
+
   Serial.begin(9600); 
 
 
 }
 
 void loop() { 
-  int a,b,c,d,e, saida;
+  int a,b,c,d,e, entrada;
   a = digitalRead(I4);
   b = digitalRead(I3);
   c = digitalRead(I2);
   d = digitalRead(I1);
   e = digitalRead(I0);
-  saida = converteDecimal(a,b,c,d,e);
-  Serial.println(saida,DEC);
-  delay(6000);
+  entrada = converteDecimal(a,b,c,d,e);
+  Serial.println(entrada,DEC);
+  delay(1000);
   VAL1 = digitalRead(CH1);
+
+  int f,g,h,i,j, saida;
+  f = digitalRead(T0);
+  g = digitalRead(T1);
+  h = digitalRead(T2);
+  i = digitalRead(T3);
+  j = digitalRead(T4);
+  saida = converteDecimal(f,g,h,i,j);
+  Serial.println(saida,DEC);
+  delay(1000);
+
+  
+
+  VAL2 = digitalRead(CHT);
 
 
   if (VAL1 == 0) {
@@ -64,7 +97,19 @@ void loop() {
   if (VAL1 == 1) {
     digitalWrite(VEM, HIGH);
     digitalWrite(VED, LOW);
+    if (VAL2 == 1) {
+      digitalWrite(LED5, HIGH);
+      // tone(B1,2500,50000); 
+      // delay(10);
+    } else {
+      digitalWrite(LED5,LOW);
+    }
+    
+
+
+
   }
+
 
 
 
